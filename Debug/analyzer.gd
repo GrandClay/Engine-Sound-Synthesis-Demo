4c7073d067ba1675071e06396757c6d22ee03ca7
GrandClay <93751010+GrandClay@users.noreply.github.com>
2024-04-28 21:19:28 -0600

    Clean up some code
    Added spectrum analyzer and fixed the audio bug when first opening the project.

---
+++ Debug/analyzer.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+
+const VU_COUNT = 60
+const FREQ_MAX = 11050.0
+const MIN_DB = 60
+const ANIMATION_SPEED = 0.1
+const HEIGHT_SCALE = 8.0
+
+@onready var color_rect = $ColorRect
+
+var spectrum
+var min_values = []
+var max_values = []
+
+func _ready():
+	spectrum = AudioServer.get_bus_effect_instance(0, 0)
+	min_values.resize(VU_COUNT)
+	min_values.fill(0.0)
+	max_values.resize(VU_COUNT)
+	max_values.fill(0.0)
+
+func _process(delta):
+	var prev_hz = 0
+	var data = []
+	for i in range(1, VU_COUNT + 1):
+		var hz = i * FREQ_MAX / VU_COUNT
+		var f = spectrum.get_magnitude_for_frequency_range(prev_hz, hz)
+		var energy = clamp((MIN_DB + linear_to_db(f.length())) / MIN_DB, 0.0, 1.0)
+		data.append(energy * HEIGHT_SCALE)
+		prev_hz = hz
+	for i in range(VU_COUNT):
+		if data[i] > max_values[i]:
+			max_values[i] = data[i]
+		else:
+			max_values[i] = lerp(max_values[i], data[i], ANIMATION_SPEED)
+		if data[i] <= 0.0:
+			min_values[i] = lerp(min_values[i], 0.0, ANIMATION_SPEED)
+	var fft = []
+	for i in range(VU_COUNT):
+		fft.append(lerp(min_values[i], max_values[i], ANIMATION_SPEED))
+	color_rect.get_material().set_shader_parameter("freq_data", fft)
--- Source/control.gd
+++ Source/control.gd
@@ -1,49 +1,47 @@
 extends Control
 
-var rpm: float = 0.0
+@onready var audio_steam_player = $AudioStreamPlayer
+@onready var sample_hz = $AudioStreamPlayer.stream.mix_rate
 
 @export_category("Engine")
-
 @export var cylinders: int = 4
 @export var strokes: int = 4
 
 @export_category("Sound Characteristics")
 @export_range(0.0, 1.0) var duty_cycle: float = 0.35
 
-@onready var audio_steam_player = $AudioStreamPlayer
-@onready var sample_hz = $AudioStreamPlayer.stream.mix_rate
-
-var pulse_hz: float = rpm/60.0
+var rpm: float = 0.0
+var pulse_hz: float = 0.0
 var phase: float = 0.0
 var playback: AudioStreamPlayback = null # Actual playback stream, assigned in _ready().
 var previous_white_noise: float = 0.0
 
-func _fill_buffer():
+func _fill_buffer() -> void:
 	var increment: float = pulse_hz / sample_hz
 
-	var to_fill = playback.get_frames_available()
+	var to_fill: int = playback.get_frames_available()
 	while to_fill > 0:
 		var pulse_wave: float = 0.0
 		for i in range(1, 10):
-			pulse_wave += (1/i) * sin(PI * i * duty_cycle) * cos(TAU * i * phase) # summation of pulse wave
+			pulse_wave += (1/i) * sin(PI * i * duty_cycle) * cos(TAU * i * phase) # Create the pulse wave with 10 iterations.
 		
-		pulse_wave = pulse_wave * (4/PI) + (2*duty_cycle) - 1 # amplitude and displacement correction
+		pulse_wave = pulse_wave * (4/PI) + (2*duty_cycle) - 1 # Amplitude and displacement correction of the pulse wave.
 		
-		var white_noise: float = randf() # generate white noise
-		var low_pass_noise: float = (white_noise + previous_white_noise) / 2 # make average pitch of white noise lowwer
-		previous_white_noise = white_noise 
+		var white_noise: float = randf() # Generate white noise.
+		var low_pass_noise: float = (white_noise + previous_white_noise) / 2 # Simple low pass filter of the white noise.
+		previous_white_noise = white_noise # White noise buffer used in the low pass filter.
 		
-		var engine_sound: float = max(0, pulse_wave) * low_pass_noise # apply pulse to the white noise
+		var engine_sound: float = max(0, pulse_wave) * low_pass_noise # Apply pulse to the white noise.
 		
 		playback.push_frame(Vector2.ONE * engine_sound) # Audio frames are stereo.
 		phase = fmod(phase + increment, 1.0)
 		to_fill -= 1
 
-func _process(_delta):
+func _process(_delta) -> void:
 	_fill_buffer()
 
-func _ready():
-	$HSlider.value = 700
+func _ready() -> void:
+	set_rpm($HSlider.value)
 	# Setting mix rate is only possible before play().
 	audio_steam_player.stream.mix_rate = sample_hz
 	audio_steam_player.play()
@@ -52,8 +50,11 @@ func _ready():
 	# as `fill_buffer` uses the `playback` member variable.
 	_fill_buffer()
 
-func _on_h_slider_value_changed(value):
+func set_rpm(new_rpm: float) -> void:
+	pulse_hz = new_rpm/60.0 # RPM to hertz conversion.
+	pulse_hz *= cylinders / (strokes / 2.0) # Multiply revolutions by combustions per revolution.
+
+func _on_h_slider_value_changed(value) -> void:
 	rpm = float(value)
-	pulse_hz = rpm/60.0 # RPM to hertz conversion
-	pulse_hz *= cylinders / (strokes / 2.0) # multiply revolutions by combustions per revolution
+	set_rpm(rpm)
 	$Label.text = "rpm: " + str(rpm)
